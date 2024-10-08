8637b0c9f74eeee3f1f482d280cebbf1e246b8db
Daniel.Kahma <you@example.com>
2024-10-08 23:07:19 +0200

    added name under "hello"

--- src/main/java/org/danielk/Main.java
+++ src/main/java/org/danielk/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+
+
+        String firstName = "Daniel";
+        String lastName = "Kahma";
+        String fullName = firstName + ' ' + lastName;
+
+        System.out.println(fullName);
     }
 
 }
